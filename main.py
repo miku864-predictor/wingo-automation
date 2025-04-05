1fdf351392004feb1181a1f0ce8b77e19c1375a8
miku864-predictor <predictionsxmiku2025@gmail.com>
2025-04-06 03:08:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
 client = gspread.authorize(creds)
-sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1fDsqMmSzI5YCnqRFq4ma3i-SFvuCfHweWBUl3HHS2jM/edit").sheet1
+sheet = client.open("Untitled spreadsheet").sheet1
 
 # Setup Chrome options
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--window-size=1920,1080")
 
 # Setup WebDriver
 driver = webdriver.Chrome(options=options)
@@ -23,7 +24,6 @@ driver.get("https://damangames.bet/#/home/AllLotteryGames/WinGo?id=1")
 
 time.sleep(10)  # Wait for page to load fully
 
-# Scrape data
 try:
     period = driver.find_element(By.CLASS_NAME, "period").text
     result = driver.find_element(By.CLASS_NAME, "lottery-number").text
